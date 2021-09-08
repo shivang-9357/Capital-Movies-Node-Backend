faf76a5c160c42df7b95201647e02902b2941c33
Shivang Yadav <shivangyadav9357@gmail.com>
2021-09-08 22:13:54 +0530

    fix cors

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -5,6 +5,8 @@ const usersController = require('../controllers/users-controllers');
 
 const router = express.Router();
 
+router.get('/', usersController.getUsers);
+
 router.post(
   '/signup',
   [
